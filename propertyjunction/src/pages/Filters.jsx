03b00b0b445af1b3574fc0f1d77f9965b6f706d5
B-Varun <bvarun662@gmail.com>
2023-11-13 02:03:33 -0800

    Adding default to filters

--- propertyjunction/src/pages/Filters.jsx
+++ propertyjunction/src/pages/Filters.jsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
-import { getFirestore, collection, query, where, getDocs,orderBy } from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+  orderBy,
+} from "firebase/firestore";
 import Spinner from "../components/Spinner";
 import ListingItem from "../components/ListingItem";
 import { toast } from "react-toastify";
@@ -9,106 +16,78 @@ export default function ApplyFilters() {
   const [loading, setLoading] = useState(false);
   const [lastFetchedListing, setLastFetchListing] = useState(null);
   const [filters, setFilters] = useState({
-    propertyType: '',
-    type: '',
-    priceMin: '',
-    priceMax: '',
-    builtAreaMax:'',
-    builtAreaMin:'',
-    bathrooms: '',
-    bedrooms: '',
-    furnish: '',
+    propertyType: "commercial",
+    type: "sale",
+    priceMin: "",
+    priceMax: "",
+    builtAreaMax: "",
+    builtAreaMin: "",
+    bathrooms: "",
+    bedrooms: "",
+    furnish: "semi",
   });
+  useEffect(() => {
+    if (filters.bedrooms === "5") {
+      console.log("Bedrooms have been updated:", filters.bedrooms);
+    }
+  }, [filters.bedrooms]);
 
   const handleFilterChange = (filterName, value) => {
-    console.log('Inside handle filter change function');
+    console.log("Inside handle filter change function");
     setFilters((prevFilters) => ({
       ...prevFilters,
       [filterName]: value,
     }));
   };
-  
+
+  async function checforDefaults() {
+    if (filters.bedrooms === "" || filters.bedrooms === " ")
+      handleFilterChange("bedrooms", ["1", "2", "3", "4", "5"]);
+    if (filters.bathrooms === "" || filters.bathrooms === " ")
+      handleFilterChange("bathrooms", ["1", "2", "3", "4", "5"]);
+  }
+
   async function applyFilters() {
     setLoading(true);
-    console.log('Inside apply filters function');
+    console.log("Inside apply filters function");
     try {
       // Perform Firestore query using the filters
       const db = getFirestore();
-      const listingsCollection = collection(db, 'listings');
-      let bathroomEmptyQ = query(listingsCollection);
-      let filteredQuery1 = query(listingsCollection);
-      let filteredQuery2 = query(listingsCollection);
-      console.log('In the try block of apply filters');
+      const listingsCollection = collection(db, "listings");
+      // let bathroomEmptyQ = query(listingsCollection);
+      let filteredQuery = query(listingsCollection);
+      // let filteredQuery2 = query(listingsCollection);
+      console.log("In the try block of apply filters");
       // Apply filters to the query
-      
 
-      if (filters.propertyType || filters.type || filters.furnish || filters.bathrooms || filters.bedrooms){
-        console.log('inside filters check condition')
-        if(filters.bathrooms == "" ){
-          console.log('inside if bathrooms is empty condition')
-          bathroomEmptyQ = query(listingsCollection,where('bathrooms','>=','0'))
-        }
-        console.log('querying the filters1')
-        filteredQuery1 = query(bathroomEmptyQ, where('propertyType', '==', filters.propertyType),
-                                                  where('type', '==', filters.type),
-                                                  where('furnished', '==', filters.furnish),
-                                                  //where('bathrooms', '==', filters.bathrooms),
-                                                  //where('bedrooms', '==', filters.bedrooms),
-                                                  );
-        console.log('querying the filters2')
+      if (
+        filters.propertyType ||
+        filters.type ||
+        filters.furnish ||
+        filters.bathrooms ||
+        filters.bedrooms
+      ) {
+        console.log("inside filters check condition");
+
+        console.log("querying the filters1");
+        filteredQuery = query(
+          listingsCollection,
+          where("propertyType", "==", filters.propertyType),
+          where("type", "==", filters.type),
+          where("furnished", "==", filters.furnish),
+          where("bedrooms", "in", filters.bedrooms),
+          where("bathrooms", "in", filters.bathrooms)
+        );
       }
+      console.log("N O BR : " + filters.bedrooms);
 
-      if (filters.priceMin || filters.priceMax){											
-        // if(filters.bathrooms.trim() == "" ){
-        //   filters.bathrooms = "0";
-        // }
-        // if(filters.bedrooms.trim() == "" ){
-        //   filters.bedrooms = "0";
-        // }
-        filteredQuery2 = query(listingsCollection,where('reqularPrice', '>=', filters.priceMin),
-                                                  where('reqularPrice', '<=', filters.priceMax));
-      }
-      
-
-      // if (filters.type) {
-      //   filteredQuery = query(listingsCollection, where('type', '==', filters.type));
-      // }
-
-      // if (filters.priceMin) {
-      //   filteredQuery = query(listingsCollection, where('regularPrice', '>=', parseInt(filters.priceMin, 10)));
-      // }
-
-      // if (filters.priceMax) {
-      //   filteredQuery = query(listingsCollection, where('regularPrice', '<=', parseInt(filters.priceMax, 10)));
-      // }
-
-      // if (filters.builtAreaMin) {
-      //   filteredQuery = query(listingsCollection, where('builtArea', '<=', parseInt(filters.builtAreaMin, 10)));
-      // }
-
-      // if (filters.builtAreaMax) {
-      //   filteredQuery = query(listingsCollection, where('builtArea', '<=', parseInt(filters.builtAreaMax, 10)));
-      // }
-
-      // if (filters.bathrooms) {
-      //   filteredQuery = query(listingsCollection, where('bathrooms', '==', filters.bathrooms));
-      // }
-
-      // if (filters.bedrooms) {
-      //   filteredQuery = query(listingsCollection, where('bedrooms', '==', filters.bedrooms));
-      // }
-      
-      // if (filters.furnish) {
-      //   filteredQuery = query(listingsCollection, where('furnished', '==', filters.furnish));
-      // }
-
-      console.log('End of filters in apply filters block');
+      console.log("End of filters in apply filters block");
 
       // Execute the query
-      const querySnapshot = await getDocs(filteredQuery2,filteredQuery1);
+      const querySnapshot = await getDocs(filteredQuery);
       const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
       setLastFetchListing(lastVisible);
-      console.log('results fetched');
+      console.log("results fetched");
 
       // Extract listing data from the query snapshot
       const listings = querySnapshot.docs.map((doc) => ({
@@ -116,26 +95,28 @@ export default function ApplyFilters() {
         data: doc.data(),
       }));
       setListings(listings);
-      console.log('Listings updated');
+      console.log("Listings updated");
       setLoading(false);
-
     } catch (error) {
       console.log(error);
-      toast.error('Error fetching listings:', error);
+      toast.error("Error fetching listings:", error);
       setLoading(false);
     }
-  };
-
-
+  }
 
   return (
-    
     <div className="max-w-7xl mx-auto pt-4 space-y-6">
-      <label className={`ml-2 mt-3  py-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}>
+      <label
+        className={`ml-2 mt-3  py-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}
+      >
         Property Type:
-        <select className={'ml-1 mt-3 bg-white border-gray-300 rounded transition ease-in-out'}
+        <select
+          className={
+            "ml-1 mt-3 bg-white border-gray-300 rounded transition ease-in-out"
+          }
           value={filters.propertyType}
-          onChange={(e) => handleFilterChange('propertyType', e.target.value)} >
+          onChange={(e) => handleFilterChange("propertyType", e.target.value)}
+        >
           <option value="">Any</option>
           <option value="house">House</option>
           <option value="apartment">Apartment</option>
@@ -144,72 +125,109 @@ export default function ApplyFilters() {
         </select>
       </label>
 
-      <label className={`ml-4 mt-3  py-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}>
+      <label
+        className={`ml-4 mt-3  py-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}
+      >
         Listing Type:
-        <select className={'ml-1 mt-3 bg-white border-gray-300 rounded transition ease-in-out'}
+        <select
+          className={
+            "ml-1 mt-3 bg-white border-gray-300 rounded transition ease-in-out"
+          }
           value={filters.type}
-          onChange={(e) => handleFilterChange('type', e.target.value)} >
+          onChange={(e) => handleFilterChange("type", e.target.value)}
+        >
           <option value="">Any</option>
           <option value="sale">Sale</option>
           <option value="rent">Rent</option>
         </select>
       </label>
 
-      <label className={`ml-2 mb-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}>
+      <label
+        className={`ml-2 mb-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}
+      >
         Price Range:
-        <input className={'ml-1 bg-white border-gray-300 rounded transition ease-in-out'}
+        <input
+          className={
+            "ml-1 bg-white border-gray-300 rounded transition ease-in-out"
+          }
           type="number"
           placeholder="Min"
           value={filters.priceMin}
-          onChange={(e) => handleFilterChange('priceMin', e.target.value)}
+          onChange={(e) => handleFilterChange("priceMin", e.target.value)}
         />
-        <input className={'ml-1 bg-white border-gray-300 rounded transition ease-in-out'}
+        <input
+          className={
+            "ml-1 bg-white border-gray-300 rounded transition ease-in-out"
+          }
           type="number"
           placeholder="Max"
           value={filters.priceMax}
-          onChange={(e) => handleFilterChange('priceMax', e.target.value)}
+          onChange={(e) => handleFilterChange("priceMax", e.target.value)}
         />
       </label>
 
-      <label className={`ml-2 mb-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}>
+      <label
+        className={`ml-2 mb-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}
+      >
         Built Area:
-        <input className={'ml-1 bg-white border-gray-300 rounded transition ease-in-out w-20'}
+        <input
+          className={
+            "ml-1 bg-white border-gray-300 rounded transition ease-in-out w-20"
+          }
           type="number"
           placeholder="Min"
           value={filters.builtAreaMin}
-          onChange={(e) => handleFilterChange('builtAreaMin', e.target.value)}
+          onChange={(e) => handleFilterChange("builtAreaMin", e.target.value)}
         />
-        <input className={'ml-1 bg-white border-gray-300 rounded transition ease-in-out w-20'}
+        <input
+          className={
+            "ml-1 bg-white border-gray-300 rounded transition ease-in-out w-20"
+          }
           type="number"
           placeholder="Max"
           value={filters.builtAreaMax}
-          onChange={(e) => handleFilterChange('builtAreaMax', e.target.value)}
+          onChange={(e) => handleFilterChange("builtAreaMax", e.target.value)}
         />
       </label>
 
-      <label className={`ml-2 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}>
+      <label
+        className={`ml-2 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}
+      >
         Bedrooms:
-        <input className={'ml-1 bg-white border-gray-300 rounded transition ease-in-out w-20'}
+        <input
+          className={
+            "ml-1 bg-white border-gray-300 rounded transition ease-in-out w-20"
+          }
           type="number"
           value={filters.bedrooms}
-          onChange={(e) => handleFilterChange('bedrooms', e.target.value)}
+          onChange={(e) => handleFilterChange("bedrooms", [e.target.value])}
         />
       </label>
 
-      <label className={`ml-2 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}>
+      <label
+        className={`ml-2 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}
+      >
         Baths:
-        <input className={'ml-1 bg-white border-gray-300 rounded transition ease-in-out w-20'}
+        <input
+          className={
+            "ml-1 bg-white border-gray-300 rounded transition ease-in-out w-20"
+          }
           type="number"
           value={filters.bathrooms}
-          onChange={(e) => handleFilterChange('bathrooms', e.target.value)}
+          onChange={(e) => handleFilterChange("bathrooms", [e.target.value])}
         />
       </label>
 
-      <label className={`ml-2 py-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}>
+      <label
+        className={`ml-2 py-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}
+      >
         Furnish:
-        <select className={'ml-1 mt-4 bg-white border-gray-300 rounded transition ease-in-out'}
+        <select
+          className={
+            "ml-1 mt-4 bg-white border-gray-300 rounded transition ease-in-out"
+          }
           value={filters.furnish}
-          onChange={(e) => handleFilterChange('furnish', e.target.value)}
+          onChange={(e) => handleFilterChange("furnish", e.target.value)}
         >
           <option value="">Any</option>
           <option value="fully">Fully</option>
@@ -218,35 +236,38 @@ export default function ApplyFilters() {
         </select>
       </label>
 
-      <button onClick={applyFilters} className="ml-2 bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800">Apply</button>
-      
+      <button
+        // onClick={abc}
+        onMouseEnter={checforDefaults}
+        onClick={applyFilters}
+        className="ml-2 bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+      >
+        Apply
+      </button>
+
       <div className="max-w-6xl mx-auto px-3">
-      <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-800 after:border-t after:flex-1 after:border-gray-800">
+        <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-800 after:border-t after:flex-1 after:border-gray-800"></div>
+        <h1 className="text-3xl text-center mt-6 font-bold mb-6">Results</h1>
+        {loading ? (
+          <Spinner />
+        ) : listings && listings.length > 0 ? (
+          <>
+            <main>
+              <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+                {listings.map((listing) => (
+                  <ListingItem
+                    key={listing.id}
+                    id={listing.id}
+                    listing={listing.data}
+                  />
+                ))}
+              </ul>
+            </main>
+          </>
+        ) : (
+          <p>No results found</p>
+        )}
       </div>
-      <h1 className="text-3xl text-center mt-6 font-bold mb-6">Results</h1>
-      {loading ? (
-        <Spinner />
-      ) : listings && listings.length > 0 ? (
-        <>
-          <main>
-            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-              {listings.map((listing) => (
-                <ListingItem
-                  key={listing.id}
-                  id={listing.id}
-                  listing={listing.data}
-                />
-              ))}
-            </ul>
-          </main>
-        </>
-         ) : (
-        <p>No results found</p>
-      )}
-        </div>
     </div>
-    
-
   );
-};
-
+}
--- propertyjunction/src/pages/Queries.jsx
+++ propertyjunction/src/pages/Queries.jsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
-import { getFirestore, collection, query, where, getDocs, orderBy, limit } from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+  orderBy,
+  limit,
+} from "firebase/firestore";
 import Spinner from "../components/Spinner";
 import ListingItem from "../components/ListingItem";
 import { toast } from "react-toastify";
@@ -8,59 +16,94 @@ export default function ApplyFilters() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(false);
   const [lastFetchedListing, setLastFetchListing] = useState(null);
-  const [selectedQuery, setSelectedQuery] = useState('query1');
-  
+  const [selectedQuery, setSelectedQuery] = useState("query1");
+
   const handleFilterChange = (value) => {
-    console.log('Inside handle filter change function');
+    console.log("Inside handle filter change function");
     setSelectedQuery(value);
   };
 
   const db = getFirestore();
-  const listingsCollection = collection(db, 'listings');
+  const listingsCollection = collection(db, "listings");
 
-  // Define query1
+  // Query1
   // Query to fetch commercial rent properties that have maximum bathrooms
   const query1Logic = () => {
-    const propertyQuery = query(listingsCollection, where("propertyType", "==", "commercial"), orderBy("bathrooms", "desc"), limit(1));
+    const propertyQuery = query(
+      listingsCollection,
+      where("propertyType", "==", "commercial"),
+      orderBy("bathrooms", "desc"),
+      limit(1)
+    );
     const propertyTypeQ = query(propertyQuery, where("type", "==", "rent"));
     return propertyTypeQ;
   };
 
-  // Define query2
+  // Query2
   // Query to get properties in hyderabad with max bedrooms and amenities that has gymnasium
   const query2Logic = () => {
     //step1: Get the maximum number of bedrooms
-    const maxBedrooms = query(listingsCollection,where("locality", "==", "Hyderabad"), orderBy("bedrooms","desc"),limit(1));
+    const maxBedrooms = query(
+      listingsCollection,
+      where("locality", "==", "Hyderabad"),
+      orderBy("bedrooms", "desc"),
+      limit(1)
+    );
     //step2: from above collection get the properties that has gymnasium
-    const offerQuery = query(maxBedrooms,where("amenities", "array-contains", "gymnasium"));
+    const offerQuery = query(
+      maxBedrooms,
+      where("amenities", "array-contains", "gymnasium")
+    );
     return offerQuery;
   };
 
-  // Define query3
+  // Query3
   // Fetch Houses on sale with offer and has more than 2 bedrooms
   const query3Logic = () => {
-    const fetchHouses = query(listingsCollection,where("propertyType", "==", "house"), where("type", "==","sale"), where("offer", "==",true),
-                                                where("bedrooms",">=", "2"),orderBy("bedrooms"));
+    const fetchHouses = query(
+      listingsCollection,
+      where("propertyType", "==", "house"),
+      where("type", "==", "sale"),
+      where("offer", "==", true),
+      where("bedrooms", ">=", "2"),
+      orderBy("bedrooms")
+    );
     return fetchHouses;
   };
 
-
+  // Query4
+  // Fetch commercial properties with specific sqft price, located in pincode, with lift, safety features - smoke detector
+  // and without offers
   const query4Logic = () => {
-    const maxBedrooms = query(listingsCollection,where("locality", "==", "Hyderabad"), orderBy("bedrooms","desc"),limit(1));
-    const offerQuery = query(maxBedrooms,where("amenities", "array-contains", "gymnasium"));
-    return offerQuery;
+    const sqftQuery = query(
+      listingsCollection,
+      where("sqftPrice", ">=", 1425),
+      where("propertyType", "==", "commercial"),
+      where("pincode", "==", "500097"),
+      where("securitySafety", "array-contains", "smokedetectors"),
+      limit(2)
+    );
+    return sqftQuery;
   };
 
+  //Query5
+  // Fetch commercial properties with lowest security deposit, and offer is false, order by security deposit.
   const query5Logic = () => {
-    const maxBedrooms = query(listingsCollection,where("locality", "==", "Hyderabad"), orderBy("bedrooms","desc"),limit(1));
-    const offerQuery = query(maxBedrooms,where("amenities", "array-contains", "gymnasium"));
-    return offerQuery;
+    const depositQ = query(
+      listingsCollection,
+      where("propertyType", "==", "commercial"),
+      where("offer", "==", false),
+      orderBy("securityDeposit", "asc"),
+      limit(3)
+    );
+    // const maxBedrooms = query(listingsCollection,where("locality", "==", "Hyderabad"), orderBy("bedrooms","desc"),limit(1));
+    // const offerQuery = query(maxBedrooms,where("amenities", "array-contains", "gymnasium"));
+    return depositQ;
   };
-
 
   async function applyFilters() {
     setLoading(true);
-    console.log('Inside apply filters function');
+    console.log("Inside apply filters function");
     try {
       const selectedQueryLogic = getQueryLogic(selectedQuery);
 
@@ -68,7 +111,7 @@ export default function ApplyFilters() {
       const querySnapshot = await getDocs(selectedQueryLogic);
       const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
       setLastFetchListing(lastVisible);
-      console.log('results fetched');
+      console.log("results fetched");
 
       // Extract listing data from the query snapshot
       const listings = querySnapshot.docs.map((doc) => ({
@@ -76,29 +119,28 @@ export default function ApplyFilters() {
         data: doc.data(),
       }));
       setListings(listings);
-      console.log('Listings updated');
+      console.log("Listings updated");
       setLoading(false);
-
     } catch (error) {
       console.log(error);
-      toast.error('Error fetching listings');
+      toast.error("Error fetching listings");
       setLoading(false);
     }
-  };
+  }
 
   const getQueryLogic = (query) => {
     switch (query) {
-      case 'query1':
+      case "query1":
         return query1Logic();
-      case 'query2':
+      case "query2":
         return query2Logic();
-      case 'query3':
+      case "query3":
         return query3Logic();
-      case 'query4':
+      case "query4":
         return query4Logic();
-      case 'query5':
+      case "query5":
         return query5Logic();
-        
+
       default:
         throw new Error(`Unsupported query: ${query}`);
     }
@@ -106,10 +148,14 @@ export default function ApplyFilters() {
 
   return (
     <div className="max-w-7xl mx-auto pt-4 space-y-6">
-      <label className={`ml-2 mt-3 py-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}>
+      <label
+        className={`ml-2 mt-3 py-4 text-sm font-semibold text-black-400 border-b-[3px] border-b-transparent`}
+      >
         QUERIES
         <select
-          className={'ml-1 mt-3 bg-white border-gray-300 rounded transition ease-in-out'}
+          className={
+            "ml-1 mt-3 bg-white border-gray-300 rounded transition ease-in-out"
+          }
           value={selectedQuery}
           onChange={(e) => handleFilterChange(e.target.value)}
         >
@@ -121,33 +167,36 @@ export default function ApplyFilters() {
         </select>
       </label>
 
-      <button onClick={applyFilters} className="ml-2 bg-blue-600 text-white px-7 py-2.5 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800">Execute Query</button>
+      <button
+        onClick={applyFilters}
+        className="ml-2 bg-blue-600 text-white px-7 py-2.5 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+      >
+        Execute Query
+      </button>
 
-      {/* Rest of your code... */}
-	  <div className="max-w-6xl mx-auto px-3">
-      <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-800 after:border-t after:flex-1 after:border-gray-800">
+      <div className="max-w-6xl mx-auto px-3">
+        <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-800 after:border-t after:flex-1 after:border-gray-800"></div>
+        <h1 className="text-3xl text-center mt-6 font-bold mb-6">Results</h1>
+        {loading ? (
+          <Spinner />
+        ) : listings && listings.length > 0 ? (
+          <>
+            <main>
+              <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                {listings.map((listing) => (
+                  <ListingItem
+                    key={listing.id}
+                    id={listing.id}
+                    listing={listing.data}
+                  />
+                ))}
+              </ul>
+            </main>
+          </>
+        ) : (
+          <p>No results found</p>
+        )}
       </div>
-      <h1 className="text-3xl text-center mt-6 font-bold mb-6">Results</h1>
-      {loading ? (
-        <Spinner />
-      ) : listings && listings.length > 0 ? (
-        <>
-          <main>
-            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-              {listings.map((listing) => (
-                <ListingItem
-                  key={listing.id}
-                  id={listing.id}
-                  listing={listing.data}
-                />
-              ))}
-            </ul>
-          </main>
-        </>
-         ) : (
-        <p>No results found</p>
-      )}
-        </div>
     </div>
   );
-};
+}
